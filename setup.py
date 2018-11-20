29ceedc84d4c041a1f1de408fdad697d122d2a0e
fatchur <fatchur.rahman1@gmail.com>
2018-11-20 18:28:29 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='mysql_utils',  # Required
+    version='0.0.1',  # Required
+    description='a simple package for handling mysql in python',  # Required
+    long_description="a simple package for handling handling mysql in python",  # Optional
+    author='Chur Chur',  # Optional
+    packages=["mysql_utils"],  # Required
+)
